66c58ec6e6674274ed4539896e71cf26cc3db6a8
Kamil Szerląg <kamilszerlag@gmail.com>
2024-03-04 20:50:16 +0100

    initial files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,25 @@
 module.exports = {
   root: true,
-  extends: ['universe/native'],
+  extends: [
+    'universe',
+    'universe/native',
+    'universe/web',
+    'universe/shared/typescript-analysis',
+  ],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.d.ts'],
+
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+  ],
+  plugins: ['react-hooks'],
+  rules: {
+    'import/order': 'off',
+  },
+  env: {
+    node: true,
+  },
 };
